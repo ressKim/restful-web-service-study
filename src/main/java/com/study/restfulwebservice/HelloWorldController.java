b11594a8b219f3e9540419eac518ac1687d8376b
ressKim <sh21dlat@gmail.com>
2021-05-31 18:05:55 +0900

    dispatcherServlet study and Path variable study

--- src/main/java/com/study/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/study/restfulwebservice/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.study.restfulwebservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,4 +19,9 @@ public class HelloWorldController {
         return new HelloWorldBean("hello World");
     }
 
+    //hello-world-bean/path-variable/{name}
+    @GetMapping(path = "hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello world, %s", name));
+    }
 }
