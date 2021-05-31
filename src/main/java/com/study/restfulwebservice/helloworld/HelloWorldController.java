0bf749a180cb17a625d92cc8cc0b9dc0f93f81ef
ressKim <sh21dlat@gmail.com>
2021-05-31 20:14:31 +0900

    user list api create - GET

--- src/main/java/com/study/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/study/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,6 +1,5 @@
 package com.study.restfulwebservice.helloworld;
 
-import com.study.restfulwebservice.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
