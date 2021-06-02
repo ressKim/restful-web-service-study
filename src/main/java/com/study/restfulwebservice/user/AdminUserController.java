78c39fe77a74b211ea1c273f6d3e3aeffd21ad2f
ressKim <sh21dlat@gmail.com>
2021-06-02 11:51:55 +0900

    param, Header 를 이용한 Api version 관리 방법 study

--- src/main/java/com/study/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/study/restfulwebservice/user/AdminUserController.java
@@ -38,7 +38,10 @@ public class AdminUserController {
     }
 
     //GET /admin/users/1 -> admin/v1/users/1
-    @GetMapping("/v1/users/{id}")
+//    @GetMapping("/v1/users/{id}")
+//    @GetMapping(value = "/users/{id}/", params = "version=1")
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json")
     public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
         User user = userDaoService.findOne(id);
 
@@ -57,7 +60,10 @@ public class AdminUserController {
         return mapping;
     }
 
-    @GetMapping("/v2/users/{id}")
+    //    @GetMapping("/v2/users/{id}")
+//    @GetMapping(value = "/users/{id}/", params = "version=2")
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json")
     public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
         User user = userDaoService.findOne(id);
 
