1403159f5924a5e31df2574ed4ba2a861ef38ec7
ressKim <sh21dlat@gmail.com>
2021-06-03 18:57:53 +0900

    jpa 를 이용해서 추가, 삭제 기능 구현

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into user values (1, sysdate(), 'user1', 'test', '123456-1111111');
-insert into user values (2, sysdate(), 'user2', 'test', '223456-1111111');
-insert into user values (3, sysdate(), 'user3', 'test', '323456-1111111');
+insert into user values (90001, sysdate(), 'user1', 'test', '123456-1111111');
+insert into user values (90002, sysdate(), 'user2', 'test', '223456-1111111');
+insert into user values (90003, sysdate(), 'user3', 'test', '323456-1111111');
