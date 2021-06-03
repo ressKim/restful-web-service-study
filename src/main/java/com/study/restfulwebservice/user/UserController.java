18b3dee1e6b4ba60fa1914425d92023ac26494e3
ressKim <sh21dlat@gmail.com>
2021-06-03 16:27:56 +0900

    security 추가 및 config 추가

--- src/main/java/com/study/restfulwebservice/user/UserController.java
+++ src/main/java/com/study/restfulwebservice/user/UserController.java
@@ -1,6 +1,6 @@
 package com.study.restfulwebservice.user;
 
-import org.hibernate.EntityMode;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -9,13 +9,14 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 public class UserController {
-    private UserDaoService userDaoService;
+    private final UserDaoService userDaoService;
 
     public UserController(UserDaoService userDaoService) {
         this.userDaoService = userDaoService;
@@ -26,22 +27,53 @@ public class UserController {
         return userDaoService.findAll();
     }
 
-    //UserController.java
-//    GET / users/1 or users/10 -- String
-    @GetMapping("/users/{id}")
-    public EntityModel<User> retrieveUsers(@PathVariable int id){
-        User user = userDaoService.findOne(id);
+    @GetMapping("/users2")
+    public ResponseEntity<CollectionModel<EntityModel<User>>> retrieveUserList2() {
+        List<EntityModel<User>> result = new ArrayList<>();
+        List<User> users = userDaoService.findAll();
 
-        if(user == null){
-            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        for (User user : users) {
+            EntityModel<User> entityModel = EntityModel.of(user);
+            entityModel.add(linkTo(methodOn(this.getClass()).retrieveAllUsers()).withSelfRel());
+
+            result.add(entityModel);
         }
 
-        EntityModel<User> model = new EntityModel<>(user);
-        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
-        model.add(linkTo.withRel("all-user"));
-
-        return model;
+        return ResponseEntity.ok(CollectionModel.of(result, linkTo(methodOn(this.getClass()).retrieveAllUsers()).withSelfRel()));
     }
+
+    //UserController.java
+//    GET / users/1 or users/10 -- String
+//    @GetMapping("/users/{id}")
+//    public EntityModel<User> retrieveUsers(@PathVariable int id) {
+//        User user = userDaoService.findOne(id);
+//
+//        if (user == null) {
+//            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+//        }
+//
+//        EntityModel<User> model = new EntityModel<>(user);
+//        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+//        model.add(linkTo.withRel("all-user"));
+//
+//        return model;
+//    }
+    // 사용자 상세 정보
+    @GetMapping("/users/{id}")
+    public ResponseEntity<EntityModel<User>> retrieveUser(@PathVariable int id) {
+        User user = userDaoService.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException("id-" + id);
+        }
+
+        EntityModel<User> entityModel = EntityModel.of(user);
+
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(linkTo.withRel("all-users"));
+        return ResponseEntity.ok(entityModel);
+    }
+
 
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
